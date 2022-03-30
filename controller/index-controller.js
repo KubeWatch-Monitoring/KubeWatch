9c91a2552eaaa6c2a35a59b43bd26f56db45e1be
Pascal Lehmann <pascal.lehmann@lehmes.ch>
2022-03-30 18:38:01 +0200

    Implement MongoDB connection test

--- controller/index-controller.js
+++ controller/index-controller.js
@@ -1,13 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.indexController = exports.IndexController = void 0;
-const exampleStore_1 = require("../services/exampleStore");
 const podStore_1 = require("../services/podStore");
 class IndexController {
     async getIndex(req, res) {
         res.render("index", {
             style: req.session.style,
-            examples: await exampleStore_1.exampleStore.getAll(req.session.display),
+            examples: undefined,
             display: req.session.display,
             pods: await podStore_1.podStore.getAllPods(),
         });
