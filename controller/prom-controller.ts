612a2b96f93f50832b0673b48fb81d478f757b84
Benjamin Plattner <benjamin.plattner@ost.ch>
2022-03-31 18:22:19 +0200

    Add prometheus library to fetch data and show on dashboard

--- controller/prom-controller.ts
+++ controller/prom-controller.ts
@@ -1,23 +1,43 @@
 import { Request, Response } from "express";
-import promClient from "prom-client";
+import { PrometheusDriver } from 'prometheus-query';
+
+const prom = new PrometheusDriver({
+  // endpoint: 'http://127.0.0.1:9090',
+  // endpoint: 'http://prometheus:9090',
+  // endpoint: "https://prometheus.demo.do.prometheus.io",
+  endpoint: "http://prometheus.monitoring:9090",
+  baseURL: "/api/v1",
+});
+
+async function retrieveInstantQuery() {
+  const instantQuery = 'up{instance="demo.do.prometheus.io:9090",job="node"}';
+  // const instantQuery = 'prometheus_target_interval_length_seconds_sum{interval="15s"}';
+  const instantQueryResponse = await prom.instantQuery(instantQuery);
+  return instantQueryResponse.result;
+}
+
+async function retrieveRangeQuery() {
+  const q = 'up';
+  const start = new Date().getTime() - 24 * 60 * 60 * 1000;
+  const end = new Date();
+  const step = 6 * 60 * 60; // 1 point every 6 hours
+  const rangeQuery = await prom.rangeQuery(q, start, end, step);
+  return rangeQuery.result;
+}
 
 export class PrometheusController {
   async getMetrics(req: Request, res: Response) {
-    res.setHeader("Content-Type", register.contentType);
-    res.send(await register.metrics());
-    // res.send(await fetch("http://localhost:9100/metrics")); // does not yet work..
-    // res.render("promView", {
-    //   style: req.session.style,
-    //   display: req.session.display,
-    //   promMetrics: await register.metrics(),
-    // });
+    console.log("getMetrics start");
+    // const test = await axios.get('http://prometheus.monitoring:9090');
+    // console.log(test);
+    res.render("promView", {
+      style: req.session.style,
+      display: req.session.display,
+      promMetrics: await retrieveRangeQuery(),
+      // promMetrics: await retrieveInstantQuery(),
+    });
+    console.log('getMetrics stop')
   }
 }
 
 export const promController = new PrometheusController();
-
-const register = new promClient.Registry();
-register.setDefaultLabels({
-  app: "monitoring-kubewatch",
-});
-promClient.collectDefaultMetrics({ register });
--- routes/prom-routes.js
+++ routes/prom-routes.js
@@ -1,10 +1,12 @@
 "use strict";
-// import express from "express";
-//
-// const router = express.Router();
-// import { promController } from "../controller/prom-controller";
-//
-// router.get("/prom-metrics", promController.getMetrics.bind(promController));
-//
-// export const promRoutes = router;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.promRoutes = void 0;
+const express_1 = __importDefault(require("express"));
+const router = express_1.default.Router();
+const prom_controller_1 = require("../controller/prom-controller");
+router.get("/prom-metrics", prom_controller_1.promController.getMetrics.bind(prom_controller_1.promController));
+exports.promRoutes = router;
 //# sourceMappingURL=prom-routes.js.map
