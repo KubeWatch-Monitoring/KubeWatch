29b29f2a4f564a5f9b35514021857bd65bf43571
Pascal Lehmann <pascal.lehmann@lehmes.ch>
2022-03-31 15:20:31 +0200

    Add webapp url to console welcome message

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 (async () => {
-  const app = (await import("./app.js")).app;
+    const app = (await import("./app.js")).app;
 
-  const PORT = process.env.PORT || 8080;
-  app.listen(PORT, () => {
-    console.log(
-      `Hello! The container started successfully and is listening for HTTP requests on ${PORT}`
-    );
-    console.log("Press Ctrl+C to quit.");
-  });
+    const PORT = process.env.PORT || 8080;
+    app.listen(PORT, () => {
+        console.log(
+            `Successfully started server at http://127.0.0.1:${PORT}`
+        );
+        console.log("Press Ctrl+C to quit.");
+    });
 })();
