99bb64b6ec05806bf6aba24f8089ea0b796578b3
Benjamin Plattner <benjamin.plattner@ost.ch>
2022-04-13 14:48:44 +0200

    Populate dashboard with static pod info

--- src/services/prometheusService.ts
+++ src/services/prometheusService.ts
@@ -10,20 +10,46 @@ export class PrometheusService {
     }
 
     async getAllPods() {
-        const getPodIdentificationsQuery = 'sum by (uid, pod) (kube_pod_info{namespace!~"kube-system|monitoring|kubernetes-dashboard"})';
+        const excludeNamespaces = "kube-system|monitoring|kubernetes-dashboard";
+        let podNamesQueryFilter = '';
+        const getPodIdentificationsQuery = `sum by (uid, pod) (kube_pod_info{namespace!~"${excludeNamespaces}"})`;
         const getPodIdentificationsResponse = await this.driver.instantQuery(getPodIdentificationsQuery);
 
-        let filterQuery = '';
-        const result = getPodIdentificationsResponse.result.map(vector => {
-            const pod = new Pod(vector.metric.labels.uid, vector.metric.labels.pod, new MetricsData(0, 0, 0));
-            filterQuery += `${pod.name}|`;
+        let allPods = getPodIdentificationsResponse.result.map(vector => {
+            podNamesQueryFilter += `${vector.metric.labels.pod}|`;  // not really useful anymore
+            return new Pod(vector.metric.labels.uid, vector.metric.labels.pod, new MetricsData(0, 0, 0));
         });
 
-        const query = 'sum by (pod, image) ({__name__=~"kube_pod_info|kube_pod_container_info", namespace!~"kube-system|monitoring|kubernetes-dashboard"})';
-        const blub = await this.driver.instantQuery(query);
-        blub.result.forEach(v => console.log(v.metric));
+        for (const pod of allPods) {
+            const podImageQuery = `sum by (pod, image) ({__name__=~"kube_pod_container_info", pod=~"${pod.name}"})`;
+            const podImageResult = await this.driver.instantQuery(podImageQuery);
+            const podImageValue = podImageResult.result.map((res) => {
+                return res.metric.labels.image;
+            })
+            pod.image = podImageValue.toString();
 
-        return result;
+            const podCPUQuery = `sum by (pod) (container_cpu_system_seconds_total{pod=~"${pod.name}"})`;
+            const podCPUResult = await this.driver.instantQuery(podCPUQuery);
+            const podCPUValue = podCPUResult.result.map((res) => {
+                return res.value.value;
+            });
+            pod.metrics.cpu = +podCPUValue;
+
+            const podMemoryQuery = `sum by (pod) (container_memory_usage_bytes{pod=~"${pod.name}"})`;
+            const podMemoryResult = await this.driver.instantQuery(podMemoryQuery);
+            const podMemoryValue = podMemoryResult.result.map((res) => {
+                return res.value.value;
+            });
+            pod.metrics.memory = (+podMemoryValue)/(2**20);  // convert to megabytes (MiB)
+
+            const podDiskQuery = `sum by (pod) (container_fs_usage_bytes{pod=~"${pod.name}"})`;
+            const podDiskResult = await this.driver.instantQuery(podDiskQuery);
+            const podDiskValue = podDiskResult.result.map((res) => {
+                return res.value.value;
+            });
+            pod.metrics.disk = (+podDiskValue)/(2**10);  // convert to kilobytes (KiB)
+        }
+        return allPods;
     }
 
     async getAllDeployments() {
