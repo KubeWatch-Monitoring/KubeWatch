0dd74e6b77a36d87ab34a06b05c50973b6461d14
Olivier Lischer <olivier.lischer@liolin.ch>
2022-04-14 08:42:46 +0200

    Working chart.js with prometheus

--- src/public/js/index.ts
+++ src/public/js/index.ts
@@ -1,3 +1,29 @@
+Chart.plugins.register(ChartDatasourcePrometheusPlugin);
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+  type: 'line',
+  plugins: [ChartDatasourcePrometheusPlugin],
+  options: {
+    plugins: {
+      'datasource-prometheus': {
+        prometheus: {
+          endpoint: "https://prometheus.demo.do.prometheus.io",
+          baseURL: "/api/v1",   // default value
+        },
+        query: 'sum by (job) (go_gc_duration_seconds)',
+        timeRange: {
+          type: 'relative',
+
+          // from 12 hours ago to now
+          start: -12 * 60 * 60 * 1000,
+          end: 0,
+        },
+      },
+    },
+  },
+});
+
 document.querySelectorAll(".btn-collapse").forEach((e) => {
   e.addEventListener("click", () => {
     if (e.parentElement) {
@@ -9,3 +35,4 @@ document.querySelectorAll(".btn-collapse").forEach((e) => {
     }
   });
 });
+
