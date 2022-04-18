e5f325c5406c3168831fb317f52c39d31115e29f
Olivier Lischer <olivier.lischer@liolin.ch>
2022-04-18 22:48:43 +0200

    Fix test

--- src/tests/unit/prometheusWatcher.specs.ts
+++ src/tests/unit/prometheusWatcher.specs.ts
@@ -4,16 +4,19 @@ import {NotificationStore} from "../../services/notificationStore";
 import {PrometheusWatcher} from "../../services/prometheusWatcher";
 import {Health, Pod} from "../../model/pod";
 import {MetricsData} from "../../model/metrics-data";
+import {SettingsStore} from "../../services/settingsStore";
 
 
 describe("PrometheusWatcher", () => {
     let watcher: PrometheusWatcher;
     let notificationStore: any;
+    let settingsStore: any;
     let prometheusService: any;
     let pod: Pod;
 
     beforeEach(() => {
         notificationStore = sinon.createStubInstance(NotificationStore);
+        settingsStore = sinon.createStubInstance(SettingsStore);
         //prometheusService = sinon.createStubInstance({});
     })
     describe("watchPrometheus", () => {
@@ -23,7 +26,8 @@ describe("PrometheusWatcher", () => {
             prometheusService = {
                 getAllPods: sinon.mock().returns([pod])
             };
-            watcher = new PrometheusWatcher(notificationStore, prometheusService);
+            settingsStore.getByName.returns({value: true});
+            watcher = new PrometheusWatcher(settingsStore, prometheusService);
             watcher.onNotification(notificationStore);
 
             watcher.checkIntervalMs = 2;
@@ -42,7 +46,8 @@ describe("PrometheusWatcher", () => {
             prometheusService = {
                 getAllPods: sinon.mock().returns([pod])
             };
-            watcher = new PrometheusWatcher(notificationStore, prometheusService);
+            settingsStore.getByName.returns({value: true});
+            watcher = new PrometheusWatcher(settingsStore, prometheusService);
             watcher.onNotification(notificationStore);
 
             watcher.checkIntervalMs = 2;
@@ -52,6 +57,26 @@ describe("PrometheusWatcher", () => {
 
             expect(prometheusService.getAllPods.called).to.be.true;
             expect(notificationStore.reactOnNotification.called).to.be.true;
+            watcher.stopWatching();
+        });
+
+        it("should not create a new notification when something is wrong but the notification is disabled", async () => {
+            pod = new Pod(0, "a", "a", new MetricsData(0, 0, 0));
+            pod.health = Health.Failed;
+            prometheusService = {
+                getAllPods: sinon.mock().returns([pod])
+            };
+            settingsStore.getByName.returns({value: false});
+            watcher = new PrometheusWatcher(settingsStore, prometheusService);
+            watcher.onNotification(notificationStore);
+
+            watcher.checkIntervalMs = 2;
+            watcher.watchPrometheus();
+
+            await new Promise(r => setTimeout(r, 10));
+
+            expect(prometheusService.getAllPods.called).to.be.true;
+            expect(notificationStore.reactOnNotification.called).to.be.false;
             watcher.stopWatching();
         });
     });
