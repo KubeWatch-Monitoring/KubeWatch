377d94e929f79429d4661a8583a056c0c57d8814
Olivier Lischer <olivier.lischer@liolin.ch>
2022-05-16 18:05:22 +0200

    Clean up deleteCharSetting

--- src/view-controllers/index-controller.ts
+++ src/view-controllers/index-controller.ts
@@ -81,20 +81,14 @@ export class IndexController {
         const URL_FAILED = `${IndexRoutes.BASE_URL}?deleteAction&failed`;
         const URL_SUCCESS = `${IndexRoutes.BASE_URL}?deleteAction&success`;
 
-        if (req.body.id === undefined) {
+        if (!this.areAllParamsAvailable({id: req.body.id})) {
             res.redirect(URL_FAILED);
             return;
         }
 
-        const idParts = req.body.id.toString().split("-");
-        if (idParts.length !=2) {
-            res.redirect(URL_FAILED);
-            return;
-        }
-        const id = idParts[1];
-        let objectId;
+        let id;
         try {
-            objectId = new ObjectId(id);
+            id = IndexController.extractMongodbId(req.body.id);
         } catch (e) {
             res.redirect(URL_FAILED);
             return;
@@ -102,7 +96,7 @@ export class IndexController {
 
         let deleteSuccessful;
         try {
-            deleteSuccessful = await req.app.chartSettingStore.deleteChartSetting(objectId);
+            deleteSuccessful = await req.app.chartSettingStore.deleteChartSetting(id);
         } catch(e) {
             this.controllerUtil.setDatabaseAvailability(false);
             res.redirect(URL_FAILED);
@@ -129,6 +123,14 @@ export class IndexController {
             return `${previous}${current[0]}=${current[1]}&`;
         }, "");
         return query.substring(0, query.length-1);
+    }
+
+    private static extractMongodbId(id: string) {
+        const idParts = id.toString().split("-");
+        if (idParts.length != 2) {
+            throw new Error("chart id is not correctly formatted")
+        }
+        return new ObjectId(idParts[1]);
     }
 }
 
