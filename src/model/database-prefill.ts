3b707fa10e0c9ae53277fde41bb45546fc75dc32
Jan Untersander <jan.untersander@ost.ch>
2022-05-23 14:24:04 +0200

    Add better default dashboards

--- src/model/database-prefill.ts
+++ src/model/database-prefill.ts
@@ -10,24 +10,40 @@ export class DatabasePrefillImpl implements DatabasePrefill {
     getChartSettings(): ChartSetting[] {
         return [
             new ChartSetting(
-                "CPU Usage (Seconds)",
-                "sum by (pod) (container_cpu_system_seconds_total{namespace!~\"kube-system|monitoring|kubernetes-dashboard\", pod!~\"\"})",
+                "CPU utilisation in %",
+                "sum(irate(container_cpu_usage_seconds_total{container!=\"POD\",image!=\"\",id=~\"/kubepods.*\"}[5m])) / scalar(sum(machine_cpu_cores)) * 100",
                 -1 * 60 * 60 * 1000,
                 0,
                 5000,
                 undefined
             ),
             new ChartSetting(
-                "Memory Usage (Bytes)",
-                "sum by (pod) (container_memory_usage_bytes{namespace!~\"kube-system|monitoring|kubernetes-dashboard\", pod!~\"\"})",
+                "CPU utilisation in % per Pod",
+                "sum by (pod) (irate(container_cpu_usage_seconds_total{container!=\"POD\",image!=\"\",id=~\"/kubepods.*\"}[5m])) / scalar(sum(machine_cpu_cores)) * 100",
                 -1 * 60 * 60 * 1000,
                 0,
                 5000,
                 undefined
             ),
             new ChartSetting(
-                "Disk Usage (Bytes)",
-                "sum by (pod) (container_fs_usage_bytes{namespace!~\"kube-system|monitoring|kubernetes-dashboard\", pod!~\"\"})",
+                "Memory utilisation in %",
+                "sum(container_memory_working_set_bytes{container!=\"POD\",container!=\"\",id=~\"/kubepods.*\"}) / scalar(sum(machine_memory_bytes)) * 100",
+                -1 * 60 * 60 * 1000,
+                0,
+                5000,
+                undefined
+            ),
+            new ChartSetting(
+                "Memory utilisation in % per Pod",
+                "sum by (pod) (container_memory_working_set_bytes{container!=\"POD\",container!=\"\",id=~\"/kubepods.*\"}) / scalar(sum(machine_memory_bytes)) * 100",
+                -1 * 60 * 60 * 1000,
+                0,
+                5000,
+                undefined
+            ),
+            new ChartSetting(
+                "Number of ready nodes",
+                "sum (kube_node_status_condition{condition=\"Ready\", status=\"true\"}==1)",
                 -1 * 60 * 60 * 1000,
                 0,
                 5000,
